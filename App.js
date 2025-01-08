cd49ed671d58dc656e288fdd5a160ca6f1e0a140
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-08 15:20:18 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+//import liraries
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+// create a component
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+//make this component available to the app
+export default App;
+
+// define your styles
+const styles = StyleSheet.create({});
