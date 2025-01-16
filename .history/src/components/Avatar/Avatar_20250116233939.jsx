be13f1a5c62ec608f2bbc633934dae1b3e4a2364
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-16 23:40:55 +0300

    Add Avatar

---
+++ .history/src/components/Avatar/Avatar_20250116233939.jsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {StoryRing} from './StoryRing';
+
+const Avatar = () => {
+  return (
+    <View>
+      <StoryRing />
+    </View>
+  );
+};
+
+export default Avatar;
+
+const styles = StyleSheet.create({});
---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,16 @@
+import {Image, StyleSheet, View} from 'react-native';
+import React from 'react';
+import {StoryRing} from './StoryRing';
+
+const Avatar = () => {
+  return (
+    <View>
+      <StoryRing />
+      <Image />
+    </View>
+  );
+};
+
+export default Avatar;
+
+const styles = StyleSheet.create({});
